aa413d8f6ac8c1544c044208f325c5ae40555969
Nima Sedaghat <n.sedaghat@gmail.com>
2025-01-18 11:50:04 -0800

    .com to .org

--- models/fetch_models.sh
+++ models/fetch_models.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-wget -N https://www.rawdataspeaks.com/wp-content/uploads/projects/astro-machines/data/model_128d_e182_i1500000.pth.tar
+wget -N https://www.rawdataspeaks.org/wp-content/uploads/projects/astro-machines/data/model_128d_e182_i1500000.pth.tar
 
